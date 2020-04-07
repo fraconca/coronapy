1d8775521ff60a4913aaa310e4efef43f47a9461
Flavio Conca <3857571+fraconca@users.noreply.github.com>
2020-04-07 20:48:48 -0300

    Create tabela_UF.py

---
+++ tabela_UF.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import pandas as pd
+
+# Emulando uma requisição ao site, baseado na inspeção do pacotes enviados ao servidor
+headers = {
+    'authority': 'xx9p7hp1p7.execute-api.us-east-1.amazonaws.com',
+    'accept': 'application/json, text/plain, */*',
+    'sec-fetch-dest': 'empty',
+    'x-parse-application-id': 'unAFkcaNDeXajurGB7LChj8SgQYS2ptm',
+    'origin': 'https://covid.saude.gov.br',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-mode': 'cors',
+    'referer': 'https://covid.saude.gov.br/',
+    'accept-language': 'en-US,en;q=0.9,pt;q=0.8',
+}
+total = requests.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalAcumulo', headers=headers)
+mapa = requests.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMapa', headers=headers)
+
+# Transformando tudo em json, para ficar mais fácil de manipular depois
+total = json.loads(total.content)['results']
+mapa = json.loads(mapa.content)['results']
+
+# Tirando algumas colunas indesejadas
+ufs = pd.DataFrame(mapa)
+ufs = ufs.drop(['objectId', 'latitude', 'longitude', 'createdAt', 'percent'], axis=1)
+print("#"*100)
+print(" ")
+print(ufs)
+print(" ")
+print("#"*100)
